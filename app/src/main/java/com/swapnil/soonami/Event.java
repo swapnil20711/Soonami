c6d0d562279ddf8931a51549f401ec1f3c706e39
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-05-25 11:35:09 +0530

    Intial commit

---
+++ app/src/main/java/com/swapnil/soonami/Event.java
@@ -0,0 +1,31 @@
+package com.swapnil.soonami;
+/**
+ * {@Event} represents an earthquake event. It holds the details
+ * of that event such as title (which contains magnitude and location
+ * of the earthquake), as well as time, and whether or not a tsunami
+ * alert was issued during the earthquake.
+ */
+public class Event {
+
+    /** Title of the earthquake event */
+    public final String title;
+
+    /** Time that the earthquake happened (in milliseconds) */
+    public final long time;
+
+    /** Whether or not a tsunami alert was issued (1 if it was issued, 0 if no alert was issued) */
+    public final int tsunamiAlert;
+
+    /**
+     * Constructs a new {@link Event}.
+     *
+     * @param eventTitle is the title of the earthquake event
+     * @param eventTime is the time the earhtquake happened
+     * @param eventTsunamiAlert is whether or not a tsunami alert was issued
+     */
+    public Event(String eventTitle, long eventTime, int eventTsunamiAlert) {
+        title = eventTitle;
+        time = eventTime;
+        tsunamiAlert = eventTsunamiAlert;
+    }
+}
